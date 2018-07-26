c5151cd3f0c5a8449016e40b7b96ad8591d553b3
Liveipool <123653079@qq.com>
2018-07-26 19:52:27 +0800

    完成一个基本的动画样式

---
+++ src/components/AnimationPage.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="animation-page">
+    <div id='leftBackground'></div>
+    <div id='rightBackground'></div>
+    <div class="name" id="first">We</div>
+    <div class="name" id="last">Trace</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimationPage',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#leftBackground {
+  position: absolute;
+  display: inline-block;
+  left: 0;
+  width: 60%;
+  height: 100%;
+  background-color: #D70C18;
+  background: linear-gradient(250deg, transparent 25%, #D70C18 0);
+  animation: backgroundToLeft 1.5s 1s 1;
+  animation-fill-mode: forwards;
+}
+
+#rightBackground {
+  position: absolute;
+  display: inline-block;
+  left: 39%;
+  width: 61%;
+  height: 100%;
+  background-color: #194EA0;
+  background: linear-gradient(70deg, transparent 25%, #194EA0 0);
+  animation: backgroundToRight 1.5s 1s 1;
+  animation-fill-mode: forwards;
+}
+
+.name {
+  position: absolute;
+  color: #fff;
+  font-size: 200px;
+  font-weight: bold;
+  font-family: fantasy;
+}
+
+#first {
+  left: 35%;
+  top: 35%;
+  animation: nameToLeft 1.5s 1s 1;
+  animation-fill-mode: forwards;
+}
+
+#last {
+  left: 49%;
+  top: 35%;
+  animation: nameToRight 1.5s 1s 1;
+  animation-fill-mode: forwards;
+}
+
+@keyframes backgroundToLeft {
+  0 {left: 0;}
+  100% {left: -100%;}
+}
+
+@keyframes backgroundToRight {
+  0 {left: 39%;}
+  100% {left: 200%;}
+}
+
+@keyframes nameToLeft {
+  0 {left: 35%;}
+  100% {left: -65%;}
+}
+
+@keyframes nameToRight {
+  0 {left: 49%;}
+  100% {left: 210%;}
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <animation-page></animation-page>
+    <input type="search" name="searchId" id="searchId" placeholder="请输入物品ID...">
+  </div>
+</template>
+
+<script>
+import AnimationPage from './AnimationPage'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  components: {
+    AnimationPage,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+
+#searchID {
+  margin: 0;
+
+}
+
+</style>
