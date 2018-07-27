f52ee59499fb2567f0bdea6dbab6942180fada14
Liveipool <123653079@qq.com>
2018-07-28 00:54:41 +0800

    增加axios依赖，准备开始重构

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App';
 import router from './router';
 
+Vue.use(VueAxios, axios);
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
