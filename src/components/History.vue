f6ea2c63ea1db80cf537c23fcd263d8de81fecf2
Liveipool <123653079@qq.com>
2018-07-27 22:18:34 +0800

    增加一点历史记录页面代码

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="history">
-    <div>历史记录</div>
+    <div class="stepsContainer" :style="{height: `${Math.ceil(blocks.length / 3) * 200}px`}">
+      <el-steps direction="vertical">
+        <el-step title="" description="2018/7"></el-step>
+        <el-step title="" description="2018/6"></el-step>
+        <el-step title="" description="2018/5"></el-step>
+        <el-step title="" description="2018/4"></el-step>
+      </el-steps>
+    </div>
   </div>
 </template>
 
 <script>
+import blocks from '@/utils/fakeData';
+
 export default {
   name: 'History',
   data() {
     return {
       msg: '',
+      blocks,
     };
   },
 };
@@ -17,4 +27,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+</style>
+
+<style>
+/*时间轴部分*/
+.history > .stepsContainer > .el-steps > .el-step > .el-step__head {
+  color: #194EA0;
+}
+.history > .stepsContainer > .el-steps > .el-step > .el-step__head > .el-step__line {
+  background-color: #194EA0;
+}
+.history > .stepsContainer > .el-steps > .el-step > .el-step__head > .el-step__icon {
+  border: 1px solid #194EA0;
+}
+
+.history > .stepsContainer > .el-steps > .el-step > .el-step__main > .el-step__description {
+  color: #194EA0;
+}
 </style>
